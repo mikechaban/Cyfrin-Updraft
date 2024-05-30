f9150a19c095c0f261d1274b1628f0c8848f016e
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-05-30 19:20:57 -0400

    added tests for FundMe

---
+++ Foundry Fundamentals/foundry-fund-me/test/FundMeTest.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    // ^of type | ^variable
+
+    function setUp() external {
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+}
