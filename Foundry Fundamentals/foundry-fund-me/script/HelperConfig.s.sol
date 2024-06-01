f4ab224de645d31d412f78b52d99b11db909bdcc
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-06-01 01:56:25 -0400

    added HelperConfig script and automatic priceFeed detection

---
+++ Foundry Fundamentals/foundry-fund-me/script/HelperConfig.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainId == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig()
+        public
+        pure
+        returns (NetworkConfig memory sepoliaNetworkConfig)
+    {
+        sepoliaNetworkConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+}
