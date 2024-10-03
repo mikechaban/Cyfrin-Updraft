277d812c631efb6d102e84881def48e4a2a1473a
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-10-03 18:52:41 -0400

    updated solidity versions for SimpleStorage

--- Foundry Fundamentals/foundry-simple-storage/script/DeploySimpleStorage.s.sol
+++ Foundry Fundamentals/foundry-simple-storage/script/DeploySimpleStorage.s.sol
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+
+pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
+        // Start sending real transactions
         vm.startBroadcast();
-        // vm keyword exists in Foundry
-
-        // any transaction that we wanna actually send we put here (inside the two cheatcodes)
 
         SimpleStorage simpleStorage = new /* creates a new contract */ SimpleStorage();
 
         vm.stopBroadcast();
-
+        // Stop sending real transactions
         return simpleStorage;
     }
 }
