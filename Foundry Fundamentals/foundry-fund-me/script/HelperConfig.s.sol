1a2dae12529c71ff16d73a557106f2f4740741b0
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-10-21 23:34:53 -0400

    created broadcast folder and moved FundMeTest

--- Foundry Fundamentals/foundry-fund-me/script/HelperConfig.s.sol
+++ Foundry Fundamentals/foundry-fund-me/script/HelperConfig.s.sol
@@ -27,11 +27,7 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig()
-        public
-        pure
-        returns (NetworkConfig memory )
-    {
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory sepoliaNetworkConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
         });
