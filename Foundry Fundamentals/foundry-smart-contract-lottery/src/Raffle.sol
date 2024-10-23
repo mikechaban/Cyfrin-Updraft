ed47e3a24e1ec7352e01960d3b83ac068bad8415
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-10-22 23:51:19 -0400

    set up smart contract lottery project

---
+++ Foundry Fundamentals/foundry-smart-contract-lottery/src/Raffle.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author Mike Chabanovskyi
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    // the declaration uint256 private immutable i_entranceFee; just creates the variable but doesn’t give it a value.
+
+    // a constructor is a special function that runs only once when a smart contract is first deployed to the blockchain. It’s used to set up the contract’s initial state, like assigning values or initializing variables. After it’s executed, it can’t be called again.
+    // in this case, the constructor is used to assign a value to that variable when the contract is deployed.
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        require(
+            msg.value >= i_entranceFee,
+            string(
+                abi.encodePacked(
+                    "Not enough ETH sent. A minimum of ",
+                    uint2str(i_entranceFee),
+                    " ETH is required."
+                )
+            )
+        );
+    }
+
+    function pickWinner() public {}
+
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    // helper function to turn uint to string
+    function uint2str(uint _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = bytes1(uint8(48 + (_i % 10)));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+}
