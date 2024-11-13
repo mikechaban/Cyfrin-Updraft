0e24a2b4b52476ace78f427bc18481fc9f4e46ca
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-11-12 23:45:25 -0500

    added subscription ID manually

--- Foundry Fundamentals/foundry-smart-contract-lottery/script/HelperConfig.s.sol
+++ Foundry Fundamentals/foundry-smart-contract-lottery/script/HelperConfig.s.sol
@@ -61,7 +61,7 @@ contract HelperConfig is CodeConstants, Script {
                 interval: 30, // 30 seconds
                 vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-                subscriptionId: 0,
+                subscriptionId: 14910510805546800735215091445691356596895366714988774482217017516503569337268,
                 callbackGasLimit: GAS_LIMIT,
                 link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
                 account: 0xfFe27aAf336A5fb593A3E81dC2fe7FEeD452ea92
