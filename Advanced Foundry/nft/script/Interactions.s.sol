684d087b8a02c1680a9dab50e84accbe5d2242f8
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-11-19 17:46:13 -0500

    added functions to interactions

--- Advanced Foundry/nft/script/Interactions.s.sol
+++ Advanced Foundry/nft/script/Interactions.s.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
 
 contract MintBasicNFT is Script {
-    function run() external {}
+    string public constant DOG =
+        "ipfs://QmaQQdaRrLWDtD1zdxzzspJ1kihWknvk8GZSPa2ueUjqVJ";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNFT",
+            block.chainid
+        );
+        mintNFTOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNFTOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNFT(contractAddress).mintNFT(DOG);
+        vm.stopBroadcast();
+    }
 }
