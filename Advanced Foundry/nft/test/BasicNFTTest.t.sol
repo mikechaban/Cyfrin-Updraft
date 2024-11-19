e4598b36d90990d2eea7387e84c5b1c8d99813a8
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-11-18 23:14:10 -0500

    added interactions

--- Advanced Foundry/nft/test/BasicNFTTest.t.sol
+++ Advanced Foundry/nft/test/BasicNFTTest.t.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test} from "../lib/forge-std/src/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {DeployBasicNFT} from "../script/DeployBasicNFT.s.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
 
 contract DeployBasicNFTTest is Test {
     DeployBasicNFT public deployer;
     BasicNFT public basicNFT;
+    address public USER = makeAddr("user");
+    string public constant DOG =
+        "ipfs://QmaQQdaRrLWDtD1zdxzzspJ1kihWknvk8GZSPa2ueUjqVJ";
 
     function setUp() public {
         deployer = new DeployBasicNFT();
@@ -21,6 +24,17 @@ contract DeployBasicNFTTest is Test {
         assert(
             keccak256(abi.encodePacked(expectedName)) ==
                 keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNFT.mintNFT(DOG);
+
+        assert(basicNFT.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(DOG)) ==
+                keccak256(abi.encodePacked(basicNFT.tokenURI(0)))
         );
     }
 }
