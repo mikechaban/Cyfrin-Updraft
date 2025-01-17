682997ad49b67a1757ad308a05962f90f7f987a0
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2025-01-16 23:58:00 -0500

    added mint and getter functions

--- Advanced Foundry/ccip-rebase-token/src/RebaseToken.sol
+++ Advanced Foundry/ccip-rebase-token/src/RebaseToken.sol
@@ -12,25 +12,48 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  */
 
 contract RebaseToken is ERC20 {
-    error RebaseToken__InterestRateCanOnlyDecrease(
-        uint256 currentInterestRate,
-        uint256 newInterestRate
-    );
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 currentInterestRate, uint256 newInterestRate);
 
     uint256 private s_interestRate = 5e10;
+    mapping (address => uint256) private s_userInterestRate;
+
+    event InterestRateSet(uint256 newInterestRate);
 
     constructor() ERC20("Rebase Token", "RBT") {}
 
-    function setInterestRate(
-        uint256 _newInterestRate
-    ) external /* onlyOwner */ {
+    /**
+     * @notice Set the interest rate in the contract
+     * @param _newInterestRate The new interest rate to set
+     * @dev The interest rate can only decrease
+     */
+    function setInterestRate(uint256 _newInterestRate) external /* onlyOwner */ {
         // Set the interest rate
-        if (_newInterestRate > s_interestRate) {
-            revert RebaseToken__InterestRateCanOnlyDecrease(
-                s_interestRate,
-                _newInterestRate
-            );
+        if (_newInterestRate >= s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
+
         s_interestRate = _newInterestRate;
+
+        emit InterestRateSet(_newInterestRate);
     }
-}
+
+    /**
+     *
+     */
+    function mint(address _to, uint256 _amount) external /* onlyVault */ {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+    }
+
+    /**
+     * @notice Gets the interest rate for a user
+     * @param _user The user to get the interest rate for
+     * @return The interest rate for the user
+     */
+    function getUserInterestRate(address _user) external view returns(uint256) {
+         return s_userInterestRate[_user];
+    }
