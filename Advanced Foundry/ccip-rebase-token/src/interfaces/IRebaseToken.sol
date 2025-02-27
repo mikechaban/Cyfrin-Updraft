e2df110f8e79d8115206266ab26db701581f3c07
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2025-02-26 23:58:46 -0500

    finished RebaseTokenPool contract

--- Advanced Foundry/ccip-rebase-token/src/interfaces/IRebaseToken.sol
+++ Advanced Foundry/ccip-rebase-token/src/interfaces/IRebaseToken.sol
@@ -7,4 +7,6 @@ interface IRebaseToken {
     function burn(address _from, uint256 _amount) external;
 
     function balanceOf(address _account) external view returns (uint256);
+
+    function getUserInterestRate(address _account) external view returns (uint256);
 }
