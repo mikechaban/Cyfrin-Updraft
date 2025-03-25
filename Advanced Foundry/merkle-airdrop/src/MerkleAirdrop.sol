1f3399f148a578c00b223fad37e11407c5a4b761
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2025-03-24 23:38:23 -0400

    added airdrop and signatures project

---
+++ Advanced Foundry/merkle-airdrop/src/MerkleAirdrop.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {SushiToken} from "./SushiToken.sol";
+
+contract MerkleAirdrop {
+    // some list of addresses
+    // allow someone in the list to claim ERC-20 tokens
+
+    
+}
