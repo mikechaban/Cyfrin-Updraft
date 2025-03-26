7e945751053d5e1be691a86a6d680abb7fd4d633
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2025-03-25 22:42:34 -0400

    added Merkle proof process

--- Advanced Foundry/merkle-airdrop/src/MerkleAirdrop.sol
+++ Advanced Foundry/merkle-airdrop/src/MerkleAirdrop.sol
@@ -1,11 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {SushiToken} from "./SushiToken.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdrop {
+    // means that we can call the functions defined in SafeERC20 on any variables of the IERC20 type
+    using SafeERC20 for IERC20;
     // some list of addresses
     // allow someone in the list to claim ERC-20 tokens
 
-    
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+
+        // if you have two inputs that produce the same hash, that's gon be a problem
+        // hashing twice to avoid collisions
+        // (second preimage attack)
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+
+        i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
