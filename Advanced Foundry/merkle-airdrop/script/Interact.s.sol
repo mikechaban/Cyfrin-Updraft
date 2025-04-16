8d0d4f3e6f9abfbf8be0fd85c1e2bbb389d9ac2f
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2025-04-15 23:10:19 -0400

    created signature and split into v, r, s

--- Advanced Foundry/merkle-airdrop/script/Interact.s.sol
+++ Advanced Foundry/merkle-airdrop/script/Interact.s.sol
@@ -12,11 +12,27 @@ contract ClaimAirdrop is Script {
     bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
     bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] proof = [PROOF_ONE, PROOF_TWO];
+    bytes private SIGNATURE =
+        hex"fbd2270e6f23fb5fe9248480c0f4be8a4e9bd77c3ad0b1333cc60b5debc511602a2a06c24085d8d7c038bad84edc53664c8ce0346caeaa3570afec0e61144dc11c";
+
+    error __ClaimAirdropScript_InvalidSignatureLength();
 
     function claimAirdrop(address airdrop) public {
         vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
         MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, proof, v, r, s);
-        vm.endBroadcast();
+        vm.stopBroadcast();
+    }
+
+    function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        if (sig.length != 65) {
+            revert __ClaimAirdropScript_InvalidSignatureLength();
+        }
+        assembly {
+            r := mload(add(sig, 32 /* or 0x20 */))
+            s := mload(add(sig, 64 /* or 0x40 */))
+            v := byte(0, mload(add(sig, 96 /* or 0x60 */)))
+        }
     }
 
     function run() external {
