8726fe204b6d25819885d487c12868eb83acbdd5
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2025-07-08 23:48:23 -0400

    finished BoxV1 and BoxV2

--- Advanced Foundry/upgradeable-smart-contracts/src/BoxV1.sol
+++ Advanced Foundry/upgradeable-smart-contracts/src/BoxV1.sol
@@ -2,9 +2,28 @@
 pragma solidity ^0.8.18;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+// storage is stored in the proxy, NOT the implementation
+
+// proxy (borrowing funcs) (number = 0) -> implementation (number = 1)
+
+// proxy -> deploy implementation -> call some "initializer" function
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    // the comment above is used to disable the warning about the constructor being unsafe for upgrades
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender); // sets the owner to msg.sender
+        __UUPSUpgradeable_init(); // initializes the UUPS upgradeable functionality
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
